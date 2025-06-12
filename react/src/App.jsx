be0a315d9123cd9d2eb2389751dddc84d097b9a4
VanBrunt, Nick <NVanBrunt@kennedy-center.org>
2025-06-12 09:35:54 -0400

    Fixed react issue

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,15 +1,45 @@
-//import { useState } from 'react'
-import Buttons from './Buttons.jsx'
 import Records from './Records.jsx'
 import Modal from './Modal.jsx'
+import { ErrorBoundary } from "react-error-boundary";
+function fallbackRender({ error, resetErrorBoundary }) {
+  // Call resetErrorBoundary() to reset the error boundary and retry the render.
+
+  return (
+    <div role="alert">
+      <p>Something went wrong:</p>
+      <pre style={{ color: "red" }}>{error.message}</pre>
+    </div>
+  );
+}
+
+function Fallback({ error, resetErrorBoundary }) {
+  // Call resetErrorBoundary() to reset the error boundary and retry the render.
+
+  return (
+    <div role="alert">
+      <p>Something went wrong:</p>
+      <pre style={{ color: "red" }}>{error.message}</pre>
+    </div>
+  );
+}
+
 
 function App() {
   return (
     <>
+      <ErrorBoundary  onError={console.error} FallbackComponent={Fallback} >
         <h2>React Test</h2>
-        <Buttons />
+        <div className="flex controls">
+            <div><input type="button" value="Add Item" onClick={() => {
+              document.getElementById('modal').classList.toggle('hidden');
+            }} /></div>
+            <div><input type="button" value="Clear Checked" onClick={() => {
+
+            }} /></div>
+        </div>
         <Records />
         <Modal />
+      </ErrorBoundary>
     </>
   )
 }
--- react/src/Records.jsx
+++ react/src/Records.jsx
@@ -2,26 +2,24 @@ import { useState, useEffect } from 'react';
 import './Records.css'
 
 function Records() {
-    const [records, setRecords] = useState(null);
-    
-    useEffect(() => {
-        fetch('/records')
+    const [records, setRecords] = useState([]);
+    const fetchRecords = async() => {
+        await fetch('/records')
             .then(response => response.json())
             .then(json => {setRecords(json) })
-            .catch(error => console.error(error));
-    }, []);
-/*
-    records fails to render after first time WTF??
-*/
+            .catch(error => console.error(error));   
+    }
+
+    useEffect(() => {fetchRecords()}, []);
+    //console.log(JSON.stringify(records))
 
     return (
     <>
-
         <div id="records">
             <table className="records">
                 <thead><tr><th>Item</th><th>Count</th><th>Price</th></tr></thead>
                 <tbody>{records.map( function(record) { 
-                return (<tr><td>{record.item}</td><td>{record.count}</td><td>{record.price}</td></tr>)})}</tbody>
+                return (<tr key={record.item}><td>{record.item}</td><td>{record.count}</td><td>{record.price}</td></tr>)})}</tbody>
             </table>
         </div>
     </>
