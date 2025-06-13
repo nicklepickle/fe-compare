3b3d0b7b895b9d0f8c96dc2ff6ad2cdaeb63d3c8
PHYSLE <PHYSLE@users.noreply.github.com>
2025-06-13 10:51:21 -0400

    Update Records.jsx

--- react/src/Records.jsx
+++ react/src/Records.jsx
@@ -1,5 +1,5 @@
-import { useState, useEffect } from 'react';
 import './Records.css'
+import Category from './Category.jsx'
 
 function Records({categories, setRecords, records}) {
     function checkItem(e) {
@@ -11,78 +11,54 @@ function Records({categories, setRecords, records}) {
 
     }
 
-  /* 
-    const [recats, setRecats] = useState([]);
-
-    const buildRecats = function() {
-       let rc = []
-        for (const cat of categories) {
-            for (const rec of records) {
-                if (rec.category == cat.categoryId) {
-                    if (!rc[cat.categoryId]) {
-                        rc[cat.categoryId] = [];
-                    }
-                    rc[cat.categoryId].push(rec);
-                }
+    function getCategoryName(id) {
+        for(const category of categories) {
+            if (category.categoryId == id) {
+                return category.category;
             }
         }
-        setRecats(rc);
+        return 'Unknown';
     }
 
-    
-    useEffect(() => {buildRecats()},[records])
+    const catrecs = []
 
-        console.log(recats.length);
-
-        recats.map( function(cat) {
-            if (cat.length > 0) {
-                cat.map( function(rec) {
-                    console.log(rec)
-                })
+    function catRecContains(cat) {
+        for(const cr of catrecs) {
+            if (cr.categoryId == cat) {
+                console.log('contains')
+                return true;
             }
-        })
+        }
+        return false;
+    }
 
-     
+    for (const r of records) {
+        if (!catRecContains(r.category)) {
+            catrecs.push({
+                category: getCategoryName(r.category),
+                categoryId: r.category,
+                records: []});
+        }
+        for(const cr of catrecs) {
+            if (cr.categoryId == r.category) {
+                cr.records.push(r)
+            }
+        }
+
+    }
+    
+    //console.log(JSON.stringify(catrec))
     return (
 
-    <>
-        <div id="records">
-            <h4>{recats.length}</h4>
-            <table className="records">
-                <thead><tr><th>Item</th><th>Count</th><th>Price</th></tr></thead>
-                <tbody>{recats.map( function(cat) { 
-                    <tr><td>CATEGORY</td></tr>
-                    cat.map( function(rec) {
-                    return (
-                    <tr key={rec.item}>
-                        <td><input type="checkbox"  onChange={checkItem} />{rec.item}</td>
-                        <td>{rec.count}</td>
-                        <td>{rec.price}</td>
-                    </tr>)}
-                    )}
-                )}</tbody>
-            </table>
-        </div>
-    </>
-    )
-        */
-    return (
         <>
             <div id="records">
                 <table className="records">
                     <thead><tr><th>Item</th><th>Count</th><th>Price</th></tr></thead>
-                    <tbody>{records.map( function(record) { 
-                    return (
-                    <tr key={record.item}>
-                        <td><input type="checkbox" name={record.item} checked={record.checked} onChange={checkItem} />{record.checked} {record.item}</td>
-                        <td>{record.count}</td>
-                        <td>${Number(record.price).toFixed(2)}</td>
-                    </tr>)})}</tbody>
+                    <tbody>{catrecs.map( function(cr) {  return (<Category key={cr.categoryId} catrec={cr} setRecords={setRecords}/>)})}</tbody>
                 </table>
             </div>
         </>
     )
-
 }
 
 export default Records
