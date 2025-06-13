e8f6511a963d5cf33872ce07d488d53de248abb4
PHYSLE <PHYSLE@users.noreply.github.com>
2025-06-13 10:47:14 -0400

    Create Category.jsx
    Added category

---
+++ react/Category.jsx
@@ -0,0 +1,29 @@
+function Category({catrec, setRecords}) {
+    function checkItem(e) {
+        console.log(e.target.name, e.target.checked)
+        fetch(`/check?item=${e.target.name}&checked=${e.target.checked}`)
+        .then(response => response.json())
+        .then(json => {setRecords(json) })
+        .catch(error => console.error(error));
+
+    }
+
+    if (catrec.records) {
+    return (
+        <>
+            <tr><th colspan="3" class="category">{catrec.category} </th></tr>
+            {catrec.records.map( function(record) { 
+              return (
+                <tr key={record.item}>
+                    <td><input type="checkbox" name={record.item} checked={record.checked} onChange={checkItem} />{record.item}</td>
+                    <td>{record.count}</td>
+                    <td>${Number(record.price).toFixed(2)}</td>
+                </tr>)})}
+
+        </>
+    )
+}
+
+}
+
+export default Category
