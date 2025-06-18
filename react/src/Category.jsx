43e4fe603703c89cf2911c7c92f1b1c2d77131a7
VanBrunt, Nick <NVanBrunt@kennedy-center.org>
2025-06-18 12:49:59 -0400

    Use svelte style marshaling

--- react/src/Category.jsx
+++ react/src/Category.jsx
@@ -1,18 +1,18 @@
-function Category({recordsByCategory, setRecords}) {
+function Category({categoryRecords, setRecords}) {
     function checkItem(e) {
         console.log(e.target.name, e.target.checked)
         fetch(`/check?item=${e.target.name}&checked=${e.target.checked}`)
-        .then(response => response.json())
-        .then(json => {setRecords(json) })
-        .catch(error => console.error(error));
+            .then(response => response.json())
+            .then(json => {setRecords(json) })
+            .catch(error => console.error(error));
 
     }
 
-    if (recordsByCategory.records) {
+    if (categoryRecords.records) {
     return (
         <>
-            <tr><th colSpan="3" className="category">{recordsByCategory.category} </th></tr>
-            {recordsByCategory.records.map( function(record) { 
+            <tr><th colSpan="3" className="category">{categoryRecords.category} </th></tr>
+            {categoryRecords.records.map( function(record) { 
               return (
                 <tr key={record.item}>
                     <td><input type="checkbox" name={record.item} checked={record.checked} onChange={checkItem} />{record.item}</td>
--- react/src/Records.jsx
+++ react/src/Records.jsx
@@ -2,45 +2,38 @@ import './Records.css'
 import Category from './Category.jsx'
 
 function Records({categories, setRecords, records}) {
-    const recordsByCategory = [];
-
-    function getCategoryName(id) {
-        for(const category of categories) {
-            if (category.categoryId == id) {
-                return category.category;
+    function getCategoryRecords(cats, recs) {
+        let catRecords = [] 
+        for(const c of cats) {
+            let catRecs = recs.filter((r) => r.category == c.categoryId);
+            if (catRecs.length > 0) {
+                let cr = catRecords.filter((cr) => cr.categoryId == c.category);
+                if (cr.length == 0) {
+                    catRecords.push({category:c.category, categoryId:c.categoryId, records:catRecs});
+                }
+                else {
+                    cr[0].push({category:c.category, categoryId:c.categoryId, records:catRecs});
+                }
             }
         }
-        return 'Unknown';
+        return catRecords;
     }
 
-    let total = 0;
-    for (const r of records.sort((a,b) => a.category - b.category)) {
-        // add the category if it isn't already there
-        if (!recordsByCategory.filter((rbc) => rbc.categoryId == r.category).length != 0) {
-            recordsByCategory.push({
-                category: getCategoryName(r.category),
-                categoryId: r.category,
-                records: []});
-        }
-        for(const rbc of recordsByCategory) {
-            if (rbc.categoryId == r.category) {
-                rbc.records.push(r)
-            }
-        }
-        total += (Number(r.price) * Number(r.count));
+    //console.log(JSON.stringify(getCategoryRecords(categories, records)))
+
+    function getTotal(recs) {
+        return recs.reduce((total, rec) => {return total + (Number(rec.price) * Number(rec.count))}, 0)
     }
     
-    //console.log(JSON.stringify(recordsByCategory))
     return (
-
         <>
             <div id="records">
                 <table className="records">
                     <thead><tr><th>Item</th><th>Count</th><th>Price</th></tr></thead>
-                    <tbody>{recordsByCategory.map( function(rbc) {  
-                        return (<Category key={rbc.categoryId} recordsByCategory={rbc} setRecords={setRecords}/>)}
+                    <tbody>{getCategoryRecords(categories, records).map( function(cr) {  
+                        return (<Category key={cr.categoryId} categoryRecords={cr} setRecords={setRecords}/>)}
                     )}
-                    <tr><td colSpan="2" className="total">Total</td><td className="total">${total.toFixed(2)}</td></tr>
+                    <tr><td colSpan="2" className="total">Total</td><td className="total">${getTotal(records).toFixed(2)}</td></tr>
                     </tbody>
                 </table>
             </div>
